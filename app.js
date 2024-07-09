8270a836f14cdea1a749fbdeb9df2c4c1b9a8c53
@mohamed.habaina <mohamed.habaina@gmail.com>
2024-07-09 23:47:18 +0200

    Handles the socket.io connection on the back server

--- app.js
+++ app.js
@@ -1,23 +1,41 @@
-const express = require('express')
-const app = express()
-const port = 3001
+const express = require('express');
 const { Server } = require("socket.io");
+const http = require('http');
+const cors = require('cors');
+const port = 3001;
+
+const app = express();
+const server = http.createServer(app);
+
+app.use(cors()); // Use cors middleware
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-
-const io = new Server({ /* options */ });
-
-io.on("connection", (socket) => {
-  // ...
+  res.send('Hello World!');
 });
 
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST']
+  }
+});
 
+io.on('connection', (socket) => {
+  console.log('User connected ', socket.id);
 
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
 
-io.listen(4000);
+  socket.on('disconnect', () => {
+    console.log('User disconnected :', socket.id);
+  });
+});
+
+server.listen(4000, () => {
+  console.log('listening on *:4000');
+});
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
